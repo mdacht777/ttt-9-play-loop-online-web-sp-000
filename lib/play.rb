21a9e489ddd2e50ce1ea5e73ad6356397b964ad4
Martin Achtermann <mdacht777@gmail.com>
2020-05-26 01:55:22 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,14 +37,10 @@ end
 
 # Define your play method below
 def play(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
+  count=1
+  while count<=9
+    turn(board)
+    count+=1
+  loop
 end
 
